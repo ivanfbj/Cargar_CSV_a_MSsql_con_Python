f6965e5f9ae1eba388ed7c6f5e0727c169077181
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2021-08-16 05:51:24 -0500

    Insert Versus Bulk Insert
    Se añade una comparación  entre la sentencia INSERT y un Bulk Insert para medir la velocidad de inserción a la base de datos, dando una resultado más optimos el Bulk Insert.
    - La sentencia de INSERT tomó 1 minuto con 44 segundo para insertar 50 mil registro en cambio el Bulk Insert le tomó menos de un segundo.

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 import pyodbc
 import pandas as pd
 from tqdm import tqdm
+import time
+import os
 
 # # Create in Microsoft SQL Server
 # CREATE TABLE [dbo].[DepartmentTest](
-# 	[DepartmentID] [smallint] NOT NULL,
+# 	[DepartmentID] [int] NOT NULL,
 # 	[Name] [nvarchar](100) NOT NULL,
 # 	[GroupName] [nvarchar](100) NOT NULL
 # ) ON [PRIMARY]
 # GO
 
+# CREATE TABLE [dbo].[DepartmentTest_bulkInsert](
+# 	[DepartmentID] [int] NOT NULL,
+# 	[Name] [nvarchar](100) NOT NULL,
+# 	[GroupName] [nvarchar](100) NOT NULL
+# ) ON [PRIMARY]
+# GO
 
 if __name__ == '__main__':
 
+    name_file = 'department_pipe_line_masivo.csv'
+    file_abspath = os.path.abspath(name_file)
+
     # insert data from csv file into dataframe.
-    df = pd.read_csv("department_pipe_line.csv", sep='|')
+    df = pd.read_csv(name_file, sep='|')
     # Some other example server values are
     # server = 'localhost\sqlexpress' # for a named instance
     # server = 'myserver,port' # to specify an alternate port
@@ -30,11 +41,31 @@ if __name__ == '__main__':
     cursor = connection.cursor()
 
     # truncate table
-    cursor.execute("TRUNCATE TABLE DepartmentTest")
+    cursor.execute('TRUNCATE TABLE DepartmentTest')
+    cursor.execute('TRUNCATE TABLE DepartmentTest_bulkInsert')
 
     # Insert Dataframe into SQL Server:
+    start_time_insert = time.strftime("%H:%M:%S")
+    print('Hora inicio Insert por medio de un ciclo:\t', start_time_insert)
+
     for index, row in tqdm(df.iterrows()):
-        cursor.execute("INSERT INTO DepartmentTest (DepartmentID,Name,GroupName) values(?,?,?)",
+        cursor.execute('INSERT INTO DepartmentTest (DepartmentID,Name,GroupName) values(?,?,?)',
                        row.DepartmentID, row.Name, row.GroupName)
     connection.commit()
+
+    end_time_insert = time.strftime("%H:%M:%S")
+    print('Hora fin Insert por medio de un ciclo:\t\t', end_time_insert)
+
+    print('##########################################################################')
+    start_time_bulk = time.strftime("%H:%M:%S")
+    print('Hora inicio bulk:\t', start_time_bulk)
+
+    cursor.execute(
+        f"BULK INSERT DepartmentTest_bulkInsert FROM '{file_abspath}' WITH(FIELDTERMINATOR='|', ROWTERMINATOR='\n', FIRSTROW=2)")
+    connection.commit()
+    
+    end_time_bulk = time.strftime("%H:%M:%S")
+    print('Hora fin bulk:\t\t', end_time_bulk)
+
     cursor.close()
+    connection.close()
